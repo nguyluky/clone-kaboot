2af43816fc681f2e8e6cc22bbd39760374784801
nguyluky <nguyluky@gmail.com>
2025-03-05 22:01:07 +0700

    add API endpoint comments for session management

--- src/index.js
+++ src/index.js
@@ -19,6 +19,14 @@ app.use(express.json())
 app.use(cookieParser())
 
 
+// /api/session/:sessionID/ => lấy thông tin phiên làm việc
+// /api/session/:sessionID/leaderboard => lấy thông tin bảng xếp hạng
+
+// chỉ dùng được khi trạng tháy là bắt đầb
+// /api/session/:sessionID/cau_hoi => gửi một câu hỏi một cách ngẫu nhiên trả về đúng hoặc sai
+// /api/session/:sessionID/tra_loi => trả lời câu hỏi
+
+
 app.listen(PORT, (err) => {
     console.log('server is runing at port ' + PORT);
 })
